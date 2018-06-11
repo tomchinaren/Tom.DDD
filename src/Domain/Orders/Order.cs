d7b4324c6cdab95485be3b1bf017aa7e4d29b204
liangjuntang <liangjuntang@mallcoo.cn>
2018-06-11 19:00:53 +0800

    1

---
+++ src/Domain/Orders/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Orders
+{
+    public class Order: IAggregateRoot
+    {
+        public long ID { get; set; }
+        private ICollection<OrderItem> _orderItems;
+
+        public void AddOrder()
+        {
+
+        }
+        public void UpdateOrderStatus()
+        {
+
+        }
+    }
+}
---
+++ src/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    /// <summary>
+    /// 创建订单
+    /// 更新订单状态
+    /// </summary>
+    public class OrderService
+    {
+        private readonly Order _order;
+        public OrderService(Order order)
+        {
+            this._order = order;
+        }
+    }
+}
